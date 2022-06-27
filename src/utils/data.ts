493a9cb8a07a45fbf0100305f33fec1658609ce4
SamX23 <skalexsong@gmail.com>
2022-06-27 23:35:36 +0700

    Update : create basic assets

---
+++ src/utils/data.ts
@@ -0,0 +1,24 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Anonim ke-1",
+      tag: "anonim-1",
+      imageUrl: "assets/images/anonim-1.jpeg",
+    },
+    {
+      id: 2,
+      name: "Anonim ke-2",
+      tag: "anonim-2",
+      imageUrl: "assets/images/anonim-2.jpeg",
+    },
+    {
+      id: 3,
+      name: "Anonim ke-3",
+      tag: "anonim-3",
+      imageUrl: "assets/images/anonim-3.jpeg",
+    },
+  ];
+};
+
+export { getData };
