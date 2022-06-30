3baa1c3c33683cd1c42e4564d78ea846efa151df
SamX23 <skalexsong@gmail.com>
2022-06-30 23:36:48 +0700

    Update : add contact input component

---
+++ src/components/ContactInput.tsx
@@ -0,0 +1,56 @@
+import { ChangeEvent, MouseEvent, useState } from "react";
+
+interface props {
+  addContact(args: { name: string; tag: string }): void;
+}
+
+const ContactInput = ({ addContact }: props) => {
+  const [state, setState] = useState({ name: "", tag: "" });
+
+  const onNameChangeEventHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        name: event.target.value,
+      };
+    });
+  };
+
+  const onTagChangeEventHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        tag: event.target.value,
+      };
+    });
+  };
+
+  const onSubmitEventHandler = (event: MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    addContact(state);
+  };
+
+  return (
+    <form className="contact-input" onSubmit={() => onSubmitEventHandler}>
+      <input
+        type="text"
+        placeholder="Nama"
+        onChange={onNameChangeEventHandler}
+        value={state.name}
+      />
+      <input
+        type="text"
+        placeholder="Tag"
+        onChange={onTagChangeEventHandler}
+        value={state.tag}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default ContactInput;
